a11e1a04b3ffbf98fb4a191045f6b7412711f100
Denis Moiseev <isdenmois@gmail.com>
2024-01-08 14:14:08 +0300

    rename app file

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,7 @@
 import 'uno.css'
 import '@unocss/reset/eric-meyer.css'
 import 'shared/ui'
-import App from './App.svelte'
+import App from './app.svelte'
 
 const app = new App({
   target: document.getElementById('app')!,
