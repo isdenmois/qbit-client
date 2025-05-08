f824fccce44a2e0a7a90d6040013234a193555e5
Denis Moiseev <isdenmois@gmail.com>
2025-05-08 22:55:21 +0300

    feat: limit current seleted

--- src/features/limits/ui/limits-block.svelte
+++ src/features/limits/ui/limits-block.svelte
@@ -6,7 +6,7 @@
   export let icon: string
   export let limit: number
 
-  const limits = [1, 2, 3, 5, 8, 10]
+  const limits = [1, 2, 3, 5, 8, 10].map((limitB) => limitB * 1024 * 1024)
 
   const dispatch = createEventDispatcher<{ limitChange: number }>()
 </script>
@@ -18,7 +18,8 @@
 
 <div class="mt-2 flex flex-wrap gap-2">
   {#each limits as limitMb}
-    <button on:click={() => dispatch('limitChange', limitMb * 1024 * 1024)}>{formatBytes(limitMb * 1024 * 1024)}</button
+    <button class:secondary={limit !== limitMb} on:click={() => dispatch('limitChange', limitMb)}>
+      {formatBytes(limitMb)}</button
     >
   {/each}
 </div>
