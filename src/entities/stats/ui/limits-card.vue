553093f010a20494eec061f0177081481ac74bbb
Denis Moiseev <isdenmois@gmail.com>
2026-08-02 14:03:49 +0300

    fix: limit card height

--- src/entities/stats/ui/limits-card.vue
+++ src/entities/stats/ui/limits-card.vue
@@ -5,7 +5,7 @@ import StatCard from './stat-card.vue'
 </script>
 
 <template>
-  <RouterLink v-if="maindata" class="not-link" to="/limits">
+  <RouterLink v-if="maindata" class="grid not-link" to="/limits">
     <StatCard title="Limits" :down="maindata.server_state.dl_rate_limit" :up="maindata.server_state.up_rate_limit" />
   </RouterLink>
 </template>
